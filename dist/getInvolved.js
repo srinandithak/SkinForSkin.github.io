8adbdccad3f1be7cbebf9a4b59cc08fac9be448c
srinandithakamath <srinandithak21@gmail.com>
2026-01-30 00:34:50 -0600

    Revert "react additions for home page"
    This reverts commit 6c59b51f66c2796646d1da3174a3f0f193162605.
    
    t#	deleted:    node_modules/@babel/types/lib/converters/toBindingIdentifierName.js.map
    	deleted:    node_modules/@babel/types/lib/modifications/flow/removeTypeDuplicates.js

--- dist/getInvolved.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("add").addEventListener("click", function() {
-    window.open("https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NmZwZjkyamFjODl1NGhqaDIyYWxubnU3OW8gc2tpbmZvcnNraW4uc2ZzQG0&tmsrc=skinforskin.sfs%40gmail.com");
-});
