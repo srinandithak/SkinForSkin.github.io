6cedda0e700b3c718d3489f420e672cf34c4a6f4
srinandithakamath <srinandithak21@gmail.com>
2026-02-20 18:06:55 -0600

    newsletter functionality

--- getInvolved.js
+++ getInvolved.js
@@ -1,8 +1,11 @@
 console.log("js connected");
-document.getElementById("add").addEventListener("click", function() {
-    window.open("https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NmZwZjkyamFjODl1NGhqaDIyYWxubnU3OW8gc2tpbmZvcnNraW4uc2ZzQG0&tmsrc=skinforskin.sfs%40gmail.com");
-});
 
+const add = document.getElementById("add");
+if(add){
+  document.getElementById("add").addEventListener("click", function() {
+      window.open("https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=NmZwZjkyamFjODl1NGhqaDIyYWxubnU3OW8gc2tpbmZvcnNraW4uc2ZzQG0&tmsrc=skinforskin.sfs%40gmail.com");
+  });
+}
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("newsletter-form");
   const message = document.getElementById("message");
@@ -11,7 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
 
     const email = document.getElementById("email").value;
-
     try {
       const res = await fetch("http://localhost:3000/subscribe", {
         method: "POST",
@@ -22,10 +24,18 @@ document.addEventListener("DOMContentLoaded", () => {
       });
 
       const data = await res.json();
+      const container = document.querySelector(".newsletter");
 
       if (res.ok) {
-        message.textContent = "Sucessfully subscribed!";
-        form.reset();
+        // message.textContent = "Sucessfully subscribed!";
+        // form.reset();
+        container.innerHTML = `
+          <div class="success-message">
+            <p>Signup successful! Thanks for joining our newsletter :)</p>
+          </div>
+        `;
+      }else if (res.status === 400) {
+        message.textContent = "Email already subscribed!"
       } else {
         message.textContent = "❌ " + data.error;
       }
